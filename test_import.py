b30ce9a97765c9acd280eb8d9702233484ac344d
jes-bro <jes@ericbrown.net>
2024-01-24 12:36:17 -0500

    Load bridge dataset

---
+++ test_import.py
@@ -0,0 +1,22 @@
+import torch
+import tensorflow_datasets as tfds
+from torch.utils.data import Dataset, DataLoader
+
+dataset = tfds.load("bridge")
+
+class TFDatasetWrapper(Dataset):
+    def __init__(self, tf_dataset):
+        self.tf_dataset = tf_dataset
+
+    def __len__(self):
+        return len(self.tf_dataset)
+
+    def __getitem__(self, idx):
+        # TensorFlow datasets yield tuples of (image, label)
+        image, label = self.tf_dataset[idx]
+        # Convert to PyTorch tensors
+        image = torch.from_numpy(image.numpy()).float()
+        label = torch.tensor(label.numpy()).long()
+        return image, label
+    
+    print("Did the thing")
